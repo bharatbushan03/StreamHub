e055f22fc3fe451ccab5bc6ca6ebcf2abdc5a05b
bharat03 <bharatbushan5320@gmail.com>
2026-05-24 22:23:28 +0530

    feat: Connect avatar and banner upload endpoints in channel routes

--- server/src/routes/channel.routes.js
+++ server/src/routes/channel.routes.js
@@ -3,13 +3,18 @@ const {
   getChannelByUsername,
   updateMyChannel,
   getChannelVideos,
-  getCreatorDashboardStats
+  getCreatorDashboardStats,
+  updateMyChannelAvatar,
+  updateMyChannelBanner
 } = require("../controllers/channel.controller");
 const { verifyJWT } = require("../middleware/auth.middleware");
+const { uploadAvatar, uploadBanner } = require("../middleware/upload.middleware");
 
 const router = express.Router();
 
 router.patch("/me", verifyJWT, updateMyChannel);
+router.patch("/me/avatar", verifyJWT, uploadAvatar, updateMyChannelAvatar);
+router.patch("/me/banner", verifyJWT, uploadBanner, updateMyChannelBanner);
 router.get("/me/dashboard", verifyJWT, getCreatorDashboardStats);
 router.get("/:username/videos", getChannelVideos);
 router.get("/:username", getChannelByUsername);
